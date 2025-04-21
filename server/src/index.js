4d2fb199a9660577a6a22c93041b57856935e0c0
clemencedoulgue <doulgueclemencekoloaide@gmail.com>
2025-04-21 15:57:59 +0000

    Update Vercel configuration to use npm instead of pnpm

--- server/src/index.js
+++ server/src/index.js
@@ -6,7 +6,7 @@ const db = require('./config/database');
 const http = require('http');
 
 const app = express();
-const startPort = process.env.PORT || 5006;
+const startPort = process.env.PORT || 5004;
 let port = startPort;
 
 // Middleware
